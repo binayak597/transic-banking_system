5de75f7e29f3ea1301e5c4c3c4f17da147f5e7f7
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2025-05-17 19:14:29 +0530

    implement left sidebar, home page and right sidebar and mobile sidebar layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,36 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+
+  const loggedIn = {firstName: "Binayak", lastName: "Mukherjee", email: "email@example.com"}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+          type="greeting"
+          title="welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Access and manage your accout efficiently"
+          />
+          <TotalBalanceBox 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.83}
+          />
+        </header>
+
+        {/* TODO */}
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar user={loggedIn}
+      transactions={[]}
+      banks={[{currentBalance: 123.59}, {currentBalance: 500}]}
+      />
+    </section>
   )
 }
 
